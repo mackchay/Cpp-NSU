5dfaa1afb96342d370cdb2fbe296e2c0b05a63c1
mackchay <56344123+mackchay@users.noreply.github.com>
2022-09-06 00:47:50 +0700

    Lab1.BigInt created

--- Lab1.BigInt/main.cpp
+++ Lab1.BigInt/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+class BigInt {
+private:
+    std::string Number;
+
+public:
+
+    BigInt() {
+        Number = "";
+    }
+
+    BigInt(int IntNumber) {
+        Number = std::to_string(IntNumber);
+    }
+
+    BigInt(std::string StrNumber) {
+        Number += StrNumber;
+    }
+};
+
 int main() {
+    BigInt();
+    BigInt(10);
+    BigInt("21234566");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
